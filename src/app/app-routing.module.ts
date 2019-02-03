aac16c60c037f7143abb42ee5e4f15567ca4f303
jtovar2 <jtovar2@student.gsu.edu>
2019-02-02 19:51:53 -0500

    adding basic media type for profiles

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import {PortfolioComponent} from './portfolio/portfolio.component';
+import {ContactComponent} from './contact/contact.component';
+import {AboutComponent} from './about/about.component';
+import {HomeComponent} from './home/home.component';
+
+
+
+const routes: Routes = [
+{ path: 'portfolio', component: PortfolioComponent },
+{ path: 'about', component: AboutComponent },
+{ path: '', component: HomeComponent },
+{ path: 'contact', component: ContactComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+ }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { NavComponent } from './nav/nav.component';
 import { FooterComponent } from './footer/footer.component';
 import { AboutComponent } from './about/about.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,9 @@ import { PortfolioComponent } from './portfolio/portfolio.component';
     NavComponent,
     FooterComponent,
     AboutComponent,
-    PortfolioComponent
+    PortfolioComponent,
+    ContactComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
